9c8eee4ecf38f8e2147616c049879f4329a792b6
Potcharapol Suteparuk <psuteparuk@gmail.com>
2015-07-29 19:45:21 -0700

    Populate feature and drop-out rate panels

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@ $(function() {
     .defer(parseJSON, 'data/course_endtime.json')
     .await(initialize);
 
+  var $courseSelector = $('#course-selector');
+  var $studentSelector = $('input[name="student-type"]');
+
   function parseJSON(path, callback) {
     d3.json(path, function(json) {
       if (json) callback(null, json);
@@ -21,7 +24,52 @@ $(function() {
     var courseList = Object.keys(course_enroll);
 
     courseList.forEach(function(courseID) {
-      $('#course-selector').append('<option value="'+courseID+'">'+courseID+'</option>');
+      $courseSelector.append('<option value="'+courseID+'">'+courseID+'</option>');
     });
+
+    populateUI(course_enroll, student_info, course_endtime);
+
+    $courseSelector.change(function() { populateUI(course_enroll, student_info, course_endtime); });
+    $studentSelector.change(function() { populateUI(course_enroll, student_info, course_endtime); });
+  }
+
+  function populateUI(course_enroll, student_info, course_endtime) {
+    var courseID = $courseSelector.val();
+    var enrollIDs = course_enroll[courseID];
+    var studentType = parseInt($('input[name="student-type"]:checked').val(), 10);
+    var student, studentID;
+
+    switch(studentType) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        studentID = enrollIDs[Math.floor(Math.random() * enrollIDs.length)];
+        student = student_info[""+studentID];
+        break;
+    }
+
+    populateFeaturePanel(student);
+    populatePredictorPanel(student);
+  }
+
+  function populateFeaturePanel(student) {
+    $('#num-events').html(student.event.length);
+    $('#num-sessions').html(student.NumberOfSessions);
+    $('#avg-session').html(student.AverageSessionLength);
+    $('#num-events-per-session').html(student.EventsPerSession);
+  }
+
+  function populatePredictorPanel(student) {
+    var dropRate = Number((student.yhat*100).toFixed(2));
+    $('#drop-out-percentage').html(dropRate);
+    if (dropRate > 50) {
+      $('.predictors').removeClass('pass');
+      $('.predictors').addClass('fail');
+    } else {
+      $('.predictors').removeClass('fail');
+      $('.predictors').addClass('pass');
+    }
   }
 });
